cc0ee7a03431fda532fdda9893dbd2d106f1210c
jwhberrios <94324519+jwhberrios@users.noreply.github.com>
2022-02-21 23:19:35 -0500

    routes updated

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ app = Flask(__name__)
 ##WELCOME ROUTE##
 #################
 @app.route('/')
+
 def welcome():
     return(
      f"Welcome to the Hawaii Climate Analysis API!<br/>"
@@ -44,6 +45,7 @@ def welcome():
 #### PRECIPITATION ROUTE #####
 ##############################
 @app.route("/api/v1.0/precipitation")
+
 def precipitation():
    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
    precipitation = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= prev_year).all()
@@ -55,6 +57,7 @@ def precipitation():
 #### STATIONS ROUTE ##########
 ##############################
 @app.route("/api/v1.0/stations")
+
 def stations():
    results = session.query(Station.station).all()
    stations = list(np.ravel(results))
@@ -65,4 +68,35 @@ def stations():
 #### MONTHLY TEMPERATURE ROUTE #####
 ####################################  
 
+@app.route("/api/v1.0/tobs")
 
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)  
+
+
+####################################
+######## STATISTICS ROUTE ##########
+####################################   
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
